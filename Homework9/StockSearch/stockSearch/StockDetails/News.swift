7522658f608bb2524015630bb6e904781761ea7a
Hao Fan <haof9611@gmail.com>
2020-11-30 15:40:45 -0800

    Define stucture of news

---
+++ Homework9/StockSearch/stockSearch/StockDetails/News.swift
@@ -0,0 +1,16 @@
+//
+//  News.swift
+//  StockSearch
+//
+//  Created by Hao Fan on 11/30/20.
+//
+
+import Foundation
+
+struct News {
+    var urlToImage: String
+    var source: String
+    var title: String
+    var url: String
+    var publishedAt: String
+}
